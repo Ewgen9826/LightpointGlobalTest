de257caf98079221a877d47fb6e89413565340c3
Eugene <kuzmich-ewgen@list.ru>
2019-11-16 16:24:51 +0300

    search model settings

--- LightpointGlobalTestApp/SearchModel/ItemsSearchModel.cs
+++ LightpointGlobalTestApp/SearchModel/ItemsSearchModel.cs
@@ -6,6 +6,12 @@ namespace LightpointGlobalTestApp.SearchModel
 {
     public class ItemsSearchModel : OrderedSearchModel<Item>
     {
+        public int[] Ids { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int? ShopId { get; set; }
+
         protected override IQueryable<Item> Ordering(IQueryable<Item> query)
         {
             switch (Predicate)
@@ -13,6 +19,15 @@ namespace LightpointGlobalTestApp.SearchModel
                 default:
                     return Order(query, x => x.Id);
             }
+        }
+
+        public override IQueryable<Item> Filter(IQueryable<Item> query)
+        {
+            if (Ids != null && Ids.Length > 0) query = query.Where(x => Ids.Contains(x.Id));
+            if (!string.IsNullOrEmpty(Name)) query = query.Where(x => x.Name.ToUpper().Contains(Name.ToUpper()));
+            if(!string.IsNullOrEmpty(Description)) query=query.Where(x=>x.Description.ToUpper().Contains(Description.ToUpper()));
+            if (ShopId.HasValue) query = query.Where(x => x.ShopId == ShopId); 
+            return base.Filter(query);
         }
 
         public SearchResult<Item> Find(IQueryable<Item> query)
--- LightpointGlobalTestApp/SearchModel/ShopsSearchModel.cs
+++ LightpointGlobalTestApp/SearchModel/ShopsSearchModel.cs
@@ -7,6 +7,10 @@ namespace LightpointGlobalTestApp.SearchModel
 {
     public class ShopsSearchModel : OrderedSearchModel<Shop>
     {
+        public int[] Ids { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
         protected override IQueryable<Shop> Ordering(IQueryable<Shop> query)
         {
             switch (Predicate)
@@ -14,6 +18,14 @@ namespace LightpointGlobalTestApp.SearchModel
                 default:
                     return Order(query, x => x.Id);
             }
+        }
+
+        public override IQueryable<Shop> Filter(IQueryable<Shop> query)
+        {
+            if (Ids != null && Ids.Length > 0) query = query.Where(x => Ids.Contains(x.Id));
+            if (!string.IsNullOrEmpty(Name)) query = query.Where(x => x.Name.ToUpper().Contains(Name.ToUpper()));
+            if (!string.IsNullOrEmpty(Address)) query = query.Where(x => x.Address.ToUpper().Contains(Address.ToUpper()));
+            return base.Filter(query);
         }
 
         public SearchResult<Shop> Find(IQueryable<Shop> query)
